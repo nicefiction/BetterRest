4135875cd0bf5ee97d4180b1cfbf96960d831968
Olivier Van hamme <nicefiction@gmail.com>
2021-04-11 08:10:03 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Olivier Van hamme on 11/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
